adf133f15ddb840ff77783d0c204a6b763018ff2
dmac24 <dinitayala@gmail.com>
2022-04-20 04:31:09 +0000

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Client configuration file (w/ Puppet)
+
+file_line { 'Declare identity file':
+path    => '/etc/ssh/ssh_config',
+line    => '    IdentityFile ~/.ssh/school',
+replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+path    => '/etc/ssh/ssh_config',
+line    => '    PasswordAuthentication no',
+replace => true,
+}
