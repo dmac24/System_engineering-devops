3e8a7800c1023b571094b22f6f82454857da0892
dmac24 <dinitayala@gmail.com>
2022-05-23 11:22:35 +0000

    fix recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,33 @@
-ansndasdj
+#!/usr/bin/python3
+"""
+Write a recursive function that queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit
+"""
+
+
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """
+    Write a recursive function that queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit
+    """
+    import requests
+
+    user = {"User-Agent": "mozilla"}
+    params = {"count": count, "after": after}
+    request = requests.get('https://www.reddit.com/r/{}/hot.json'
+                           .format(subreddit), headers=user,
+                           allow_redirects=False, params=params)
+    if request.status_code >= 400:
+        return None
+
+    h_list = hot_list + [child.get("data").get("title")
+                         for child in request.json()
+                         .get("data")
+                         .get("children")]
+
+    info = request.json()
+    if info.get("data").get("after") is None:
+        return h_list
+
+    return recurse(subreddit, h_list, info.get("data").get("count"),
+                   info.get("data").get("after"))ansndasdj
